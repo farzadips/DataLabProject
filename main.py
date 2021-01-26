cfb59b3837528ef0e2594b3a25931c6a4f556019
Farzad ips <imanpourfarzad@gmail.com>
2021-01-26 15:28:01 +0100

    reading tsv files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 df_dev = pd.read_csv('dev.tsv',sep='\t')
-df_eval = pd.read_csv('/eval.tsv',sep='t')
+df_eval = pd.read_csv('eval.tsv',sep='\t')
 
 print(df_dev)
 print(df_eval)
